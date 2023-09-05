9a27b866fa43900b0805eb36c1f1cb268d27760f
Vera <solnce288888@gmail.com>
2023-09-05 22:58:41 +0300

    some_changes

--- hw4_4.py
+++ hw4_4.py
@@ -22,24 +22,30 @@ class ATM:
         self._balance = 0
         self.count = 0
         self.id = id(self)
+        self.history = []
 
     # пополнение баланса счета
     def top_up_balance(self, summ):
         if self._balance > 5_000_000:
+            self.history.append(f"tax_rich 10% +{self._balance * 0.1:.2f}")
             self._balance *= 0.9
         if summ % 50:
             print(f'{dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S-%f")} Ошибка! Введите сумму пополнения, кратную 50 у.е')
         else:
             self._balance += summ
+            self.history.append(f"up +{summ}")
             self.count += 1
-        if not self.count % 3:
+        if not self.count % 3 and self.count > 0:
             self._balance *= 1.03
+            self.history.append(f"interest for 3d transaction +{self._balance * 0.03:.2f}")
         print(f'{dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S-%f")} Баланс счета {self._balance:.2f} у.е')
+
         self.write_to_file()
 
     # снятие денег со счета
     def withdraw(self, summ):
         if self._balance > 5_000_000:
+            self.history.append(f"tax_rich 10% +{self._balance * 0.1:.2f}")
             self._balance *= 0.9
         if summ % 50:
             print(f'{dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S-%f")} Ошибка! Введите сумму снятия, кратную 50 у.е')
@@ -47,15 +53,19 @@ class ATM:
             print(f'{dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S-%f")} Ошибка! На счете недостаточно средств')
         else:
             self._balance -= summ
+            self.history.append(f"withdraw -{summ}")
             if summ * 1.5 / 100 < 30:
-                self._balance -= 30
+                fee = 30
             elif summ * 1.5 / 100 > 600:
-                self._balance -= 600
+                fee = 600
             else:
-                self._balance -= summ * 1.5 / 100
+                fee = summ * 1.5 / 100
+            self._balance -= fee
+            self.history.append(f"withdraw fee -{fee}")
             self.count += 1
-        if not self.count % 3:
+        if not self.count % 3 and self.count > 0:
             self._balance *= 1.03
+            self.history.append(f"interest for 3d transaction +{self._balance * 0.03:.2f}")
         print(f'{dt.datetime.now().strftime("%d.%m.%Y %H:%M:%S-%f")} Баланс счета {self._balance:.2f} у.е.')
         self.write_to_file()
     def write_to_file(self):
@@ -80,4 +90,5 @@ account1.withdraw(1_000_000) #операция №5 снятие 1_000_000 у.е
 account1.withdraw(1_000_000) #операция №6 снятие 1_000_000 у.е. Результат 3_508_642.19 - 1_000_000 - 600(т.к. 1.5% = 15_000) = 2_508_042.19 у.е.  + 3% (за 6-ю операцию) = 2_583_283.46 у.е.
 account1.withdraw(1_000_000) #операция №7 снятие 1_000_000 у.е. Результат 2_583_283.46 - 1_000_000 - 600(т.к. 1.5% = 15_000) = 1_582_683.46 у.е.
 print(account1.count) #количество успешных операций пополнения и снятия
+print(*account1.history, sep='\n') #история движения денежных средств
 account1.exit()
