99ed925f5a346134416d757990183a2aaff46d04
Vera <solnce288888@gmail.com>
2023-09-09 19:43:14 +0300

    third_task_add

--- hw5_3.py
+++ hw5_3.py
@@ -3,10 +3,23 @@
 элементов: путь, имя файла, расширение файла."""
 
 import re
-def path_name_ext(text):
+import os
+def path_name_ext_re(text):
     m = re.match(r"(^.+[\\/])(\w+)\.([a-zA-Z]{2,}$)", text)
     return m.group(1), m.group(2), m.group(3)
 
-print(path_name_ext('C:\\Users\Veronika\Desktop\\bootcamp.csproj'))
-print(path_name_ext('C:\\Users\Veronika\PycharmProjects\pythonProject\\venv\Scripts\python.exe'))
-print(path_name_ext("D:/Autotests/My first project/Studying project/Tests/Tests results/results.txt"))
+
+def path_name_ext_os(path):
+    filepath, file_extension = os.path.splitext(path)
+    dirname, filename = os.path.split(filepath)
+    return (dirname, filename, file_extension)
+
+
+
+print(path_name_ext_re('C:\\Users\Veronika\Desktop\\bootcamp.csproj'))
+print(path_name_ext_re('C:\\Users\Veronika\PycharmProjects\pythonProject\\venv\Scripts\python.exe'))
+print(path_name_ext_re("D:/Autotests/My first project/Studying project/Tests/Tests results/results.txt"))
+
+print(path_name_ext_os('C:\\Users\Veronika\Desktop\\bootcamp.csproj'))
+print(path_name_ext_os('C:\\Users\Veronika\PycharmProjects\pythonProject\\venv\Scripts\python.exe'))
+print(path_name_ext_os("D:/Autotests/My first project/Studying project/Tests/Tests results/results.txt"))
